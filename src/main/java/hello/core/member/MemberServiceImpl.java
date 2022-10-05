de3f9308484de0eacd98cce13ffed39cf7368265
Juice <3221ju@gmail.com>
2022-10-06 07:52:35 +0900

    [STU-12] 멤버 기능 추가

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,24 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    private MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    static MemberServiceImpl useMemory() {
+        return new MemberServiceImpl(new MemberRepositoryForMemory());
+    }
+
+    @Override
+    public void join(Member member) {
+        this.memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return this.memberRepository.findById(memberId);
+    }
+}
