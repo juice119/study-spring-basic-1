2c77fc6b39d4f865176bd6bf2aa44bd7e591d78c
Juice <3221ju@gmail.com>
2022-10-07 07:17:03 +0900

    [STU-13] APP config 로 DI 추가

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -1,15 +1,24 @@
 package hello.core;
 
 import hello.core.member.*;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberServiceImpl memberService = MemberServiceImpl.useMemory();
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
         Member tester = new Member(1L, "tester", Grade.BASIC);
         memberService.join(tester);
 
         Member findMember = memberService.findMember(tester.getId());
+        Order order = orderService.createOrder(findMember.getId(), "testItem", 1000);
+
         System.out.println("new Member Id= " + tester.getName());
-        System.out.println("find Member Id= " + findMember.getName());
+        System.out.println("find Member Id= " + findMember.getName() + "order = " + order.toString());
+
     }
 }
--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,6 +1,9 @@
 package hello.core.order;
 
-import hello.core.member.*;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberRepositoryForMemory;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,11 +13,10 @@ import static org.hamcrest.core.Is.is;
 class OrderServiceTest {
 
     MemberRepositoryForMemory memberRepository = new MemberRepositoryForMemory();
-    OrderService orderService = new OrderServiceImpl();
+    OrderService orderService = new OrderServiceImpl(memberRepository, new RateDiscountPolicy());
 
     @Test
     void createOrder() {
-
         // given
         Long memberId = 1L;
         Member member = new Member(memberId, "'memberA", Grade.VIP);
@@ -26,6 +28,6 @@ class OrderServiceTest {
         Order order = orderService.createOrder(memberId, itemName, itemPrice);
 
         // then
-        assertThat(order.getDiscountPrice(), is(equalTo(1000)));
+        assertThat(order.getDiscountPrice(), is(equalTo(100)));
     }
 }
