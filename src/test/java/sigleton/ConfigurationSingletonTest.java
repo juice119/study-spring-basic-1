9b904e8500f407ce7ef5b5d1948f4b304b7bbcf3
Juice <3221ju@gmail.com>
2022-10-11 22:33:11 +0900

    [STU-14] Configure 어노테이션이 등록되지 않은 경우 의존관계 주입시 싱글톤을 보장하지 않는다.

--- src/test/java/sigleton/ConfigurationSingletonTest.java
+++ src/test/java/sigleton/ConfigurationSingletonTest.java
@@ -4,7 +4,9 @@ import hello.core.AppConfig;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,5 +27,34 @@ public class ConfigurationSingletonTest {
 
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        //AppConfig도 스프링 빈으로 등록된다.
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        //출력: bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$bd479d70
+    }
+
+    @Test
+    @DisplayName("Configure 어노테이션이 등록되지 않은 경우 의존관계 주입시 싱글톤을 보장하지 않는다.")
+    void nonConfigureAnnotationContext() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(NonConfigureAppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean(MemberRepository.class);
+
+        System.out.println("memberService -> memberRepository = " +
+                memberService.getMemberRepository());
+        System.out.println("orderService -> memberRepository = " +
+                orderService.getMemberRepository());
+        System.out.println("memberRepository = " + memberRepository);
+
+        assertThat(memberService.getMemberRepository()).isNotSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isNotSameAs(memberRepository);
     }
 }
---
+++ src/test/java/sigleton/NonConfigureAppConfig.java
@@ -0,0 +1,37 @@
+package sigleton;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberRepositoryForMemory;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+
+public class NonConfigureAppConfig {
+
+    @Bean
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
+        return new MemberRepositoryForMemory();
+    }
+
+    @Bean
+    public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public OrderService orderService() {
+        System.out.println("call AppConfig.orderServices");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+}
