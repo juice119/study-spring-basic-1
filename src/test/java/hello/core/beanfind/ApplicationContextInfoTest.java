79b2363532c1dec02c9fa2626688ad07b98e4b04
Juice <3221ju@gmail.com>
2022-10-11 21:11:33 +0900

    [STU-13] AnnotationConfigApplicationContext 예제

---
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,22 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanName : beanDefinitionNames) {
+            System.out.println("name=" + beanName + ", object=" + ac.getBean(beanName));
+        }
+    }
+
+}
